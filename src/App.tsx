2dfd4a25c008b81f78ffe074ee6d5af57f3b63b8
DXVIL デビル <musicbyblakk@gmail.com>
2023-03-28 22:49:34 +0200

    update form now

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,14 @@ import React from 'react'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MainElement, AboutElement, ShopElement } from './components';
-import { Box } from '@mui/system'
 
 import "bootstrap/dist/js/bootstrap.bundle.min"
 import "./style/global.scss"
-
-import bgVideo from './assets/bg.mp4'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <video autoPlay muted loop id='bgVideo'>
-        <source src={bgVideo} type={'video/mp4'} />
-      </video>
-      <Box id={'bgVideo'} sx={{ background: '#AA0000', mixBlendMode: 'darken' }} />
       <Routes>
         <Route path="/" element={<MainElement />} />
         <Route path="/shop" element={<ShopElement />} />
--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,11 +1,11 @@
 import { Paper, Typography } from '@mui/material'
-import FolderIcon from '@mui/icons-material/Folder';
 import React from 'react'
 import { Box } from '@mui/system';
 
 const NavbarComponent = ({routerDir}) => {
+  // SET SM TO HAVE / IN TOP AND NAV UNDER 
   return (
-    <Paper sx={{padding: 2, backdropFilter: 'blur(5px)', background: '#111D', color: '#fff', minWidth: "3.25rem", flexGrow: 1, alignItems: "center", display: "flex"}}>
+    <Paper sx={{padding: 2, backdropFilter: 'blur(5px)', background: '#111D', color: '#fff', minWidth: "3.25rem", alignItems: "center", display: "flex"}}> 
       <Box sx={{fontSize: '2rem', fontFamily: 'VT323'}}>
           {`/${routerDir ? `${routerDir}` : ""}`}
       </Box>
